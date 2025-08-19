a27ec9506f307d4ea5bbdec35ffd6237f4c6a724
Alex Bond <alexej.bondarik@gmail.com>
2025-08-19 16:41:27 +0200

    HW-5: complete + classWork

--- homework-solutions/js/hw-5/task-1.js
+++ homework-solutions/js/hw-5/task-1.js
@@ -1,30 +1,46 @@
-/**
- * Сложить строку с четными числами от 10 до 0, разделенными `-` в переменную evenNumbersResult.
- * Переменная для результата `evenNumbersResult` уже создана и содержит пустую строку.
- * Ожидаемый результат: '10-8-6-4-2-0'
- */
+// /**
+//  * Сложить строку с четными числами от 10 до 0, разделенными `-` в переменную evenNumbersResult.
+//  * Переменная для результата `evenNumbersResult` уже создана и содержит пустую строку.
+//  * Ожидаемый результат: '10-8-6-4-2-0'
+//  */
 
-let evenNumbersResult = '';
+// let evenNumbersResult = '';
+// for (let i = 10; i >= 0; i -= 2) {
+//   evenNumbersResult += i + (i > 0 ? '-' : '');
+// }
+// console.log(evenNumbersResult);
 
-/**
- * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
- * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
- * Ожидаемый результат:
- * :)
- * :):)
- * :):):)
- * :):):):)
- * :):):):):)
- */
+// /**
+//  * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
+//  * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
+//  * Ожидаемый результат:
+//  * :)
+//  * :):)
+//  * :):):)
+//  * :):):):)
+//  * :):):):):)
+//  */
 
-let smilePatternResult = '';
+ let smilePatternResult = '';
+for (let i = 1; i <= 5; i++) {
+  smilePatternResult += ':)'.repeat(i) + '\n';
+}
+console.log(smilePatternResult);
 
 /**
  * Заменить все пробелы в переменной text на "1".
  * Переменная для результата `replaceSpacesWithOneResult` уже создана и содержит пустую строку.
  * Ожидаемый результат: 'Hello!1I1am1a1JS1student!'
  */
+
+
+
 const text = 'Hello! I am a JS student!';
 let replaceSpacesWithOneResult = '';
+replaceSpacesWithOneResult = text.replaceAll(' ', '1')
+console.log(replaceSpacesWithOneResult);
 
-export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
+
+
+// // export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
+
